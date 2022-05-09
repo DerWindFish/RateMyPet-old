8495aa61e33be317d11a8412e9658293baa1096c
Mark Ryan <markrryan99@gmail.com>
2022-05-09 08:48:15 -0700

    getting front end ready for auth

--- ratemypetfrontend/src/App.js
+++ ratemypetfrontend/src/App.js
@@ -1,8 +1,16 @@
 // import './App.css';
-import React from 'react';
+import React from 'react'
+import { useState, useEffect } from 'react'
+// import { CheckSession } from './services/Auth'
 import Nav from './components/Nav'
 
 function App() {
+
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+  const [anime, setAnime] = useState([])
+
+
   return (
     
     <div className="App">
--- ratemypetfrontend/src/components/Nav.jsx
+++ ratemypetfrontend/src/components/Nav.jsx
@@ -1,16 +1,59 @@
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
-const Nav = () => {
+// const Nav = () => {
 
-    return (
-        <nav className="nav-bar">
-            <h3>RateMyPet</h3>
-            <Link to='/dashboard'>Dashboard</Link>
-            <Link to='/ratedpets'>To The Pets</Link>
-            <Link to='/userprofile'>User Profile</Link>
-            <Link to='logout'>Log Out</Link>
-        </nav>
+//     return (
+//         <nav className="nav-bar">
+//             <h3>RateMyPet</h3>
+//             <Link to='/dashboard'>Dashboard</Link>
+//             <Link to='/ratedpets'>To The Pets</Link>
+//             <Link to='/userprofile'>User Profile</Link>
+//             <Link to='logout'>Log Out</Link>
+//         </nav>
+//     )
+// }
+
+// export default Nav
+
+import { Link } from 'react-router-dom'
+
+const Nav = ({ authenticated, user, handleLogOut }) => {
+  let authenticatedOptions
+  if (user) {
+    authenticatedOptions = (
+      <nav className='nav-bar'>
+        <h3>Welcome {user.username}!</h3>
+        <Link to="/dashboard">Dashboard</Link>
+		<Link to="/userprofile">My Profile</Link>
+		<Link to="/pets">Show Me The Pets!</Link>
+        <Link onClick={handleLogOut} to="/">Sign Out</Link>
+      </nav>
     )
+  }
+
+  const publicOptions = (
+    <nav className='nav-bar'>
+      <Link to="/">Home</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/signin">Sign In</Link>
+    </nav>
+  )
+
+
+  return (
+    <header>
+      <Link to="/">
+        <div className="logo-wrapper" alt="logo">
+          {/* <img
+            className="logo"
+            alt='logo'
+            src="https://i.imgur.com/UuiW73M.png"
+          /> */}
+        </div>
+      </Link>
+      {authenticated && user ? authenticatedOptions : publicOptions}
+    </header>
+  )
 }
 
 export default Nav
